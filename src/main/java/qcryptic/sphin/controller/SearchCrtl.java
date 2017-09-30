227d6c6ecfae0b6076ff24a12afdb5889bb7f10e
qcryptic <kylerump@outlook.com>
2017-09-30 13:50:23 -0500

    Add error pages, rename home page to search page

--- src/main/java/qcryptic/sphin/controller/SearchCrtl.java
+++ src/main/java/qcryptic/sphin/controller/SearchCrtl.java
@@ -5,16 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by Kyle on 9/30/2017.
+ * Created by Kyle on 9/26/2017.
  */
 @Controller
-public class SettingsCtrl {
+public class SearchCrtl {
 
-    @RequestMapping(value = {"/settings"})
+    @RequestMapping(value = {"/", "/search"})
     public String home(Model model) {
-        model.addAttribute("info", "Settings Page");
-        model.addAttribute("pageName", "settings");
-        return "pages/settings";
+        model.addAttribute("info", "Search Page");
+        model.addAttribute("pageName", "search");
+        return "pages/search";
     }
 
 }
